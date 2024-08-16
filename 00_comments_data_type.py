594e7402cc137e7adc500adaeb3322fb7f561403
Erick Moo <erickadrielmoocalan498@gmail.com>
2024-08-16 15:28:48 -0600

    Add file "00"

---
+++ 00_comments_data_type.py
@@ -0,0 +1,12 @@
+#Coment 1
+
+'''
+    Coment 2
+'''
+
+#Check the data type
+print(type("Cadena de texto")) #String
+print(type(5)) #Integer
+print(type(1.5)) #Float
+print(type(3 + 1j)) #Complex
+print(type(True)) #Bool
